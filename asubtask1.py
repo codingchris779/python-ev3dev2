7e3c69a34a31f198adc834cca11cf6a2c87f9e12
Christopher McKenzie <mckenzcp@mail.uc.edu>
2022-02-11 13:08:55 -0500

    test2

---
+++ asubtask1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from time import sleep
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+from ev3dev2.sound import Sound
+from time import sleep
+sound = Sound()
+sound.beep()
+# leftDrive = LargeMotor(OUTPUT_A) 
+# rightDrive = LargeMotor(OUTPUT_B) 
+# print("test")  #defining a large motor
+# leftDrive.on_for_seconds(speed=SpeedRPS(-2), seconds=3)
+# rightDrive.on_for_seconds(speed=SpeedRPS(2), seconds=3)
+# print("test")
+# sleep(100)
--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+from time import sleep
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
-
-leftDrive = LargeMotor(OUTPUT_A) 
-rightDrive = LargeMotor(OUTPUT_B)   #defining a large motor
-leftDrive.COMMAND_RUN_TIMED(20)
-leftDrive.COMMAND_RUN_TIMED(20)
+from ev3dev2.sound import Sound
+from time import sleep
+sound = Sound()
+sound.beep()
